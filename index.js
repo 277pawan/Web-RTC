3e2da20c0cc16cd17e82c2ab00b39a4effd04f82
Pawan Bisht <124587748+277pawan@users.noreply.github.com>
2025-02-04 12:25:21 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,47 +1,69 @@
-const { Server } = require("socket.io");
-const express = require("express");
-const cors = require("cors");
-const app = express();
-
-app.use(
-  cors({
-    origin: ["https://0e5d-122-186-71-238.ngrok-free.app/"], // Allow both local and deployed frontend
-    methods: "GET,POST,PUT,DELETE",
-    credentials: true, // If using cookies or authentication
-  })
-);
-
-require("dotenv").config();
-const io = new Server(process.env.PORT, {
-  cors: true,
-});
+// api/socket.js
+import { Server } from "socket.io";
 
 const emailToSocketId = new Map();
 const socketIdToEmail = new Map();
-io.on("connection", (socket) => {
-  console.log("Socket Connected:", socket.id);
-  socket.on("roomjoin", (data) => {
-    const { email, room } = data;
-    emailToSocketId.set(email, socket.id);
-    socketIdToEmail.set(socket.id, email);
-    io.to(room).emit("user:joined", { email, id: socket.id });
-    socket.join(room);
-    io.to(socket.id).emit("room:join", data);
-  });
-  socket.on("user:call", ({ to, offer }) => {
-    io.to(to).emit("incoming:call", { from: socket.id, offer });
-  });
-  socket.on("call:accepted", ({ to, ans }) => {
-    io.to(to).emit("call:accepted", { from: socket.id, ans });
-  });
 
-  socket.on("peer:nego:needed", ({ to, offer }) => {
-    console.log("peer:nego:needed", offer);
-    io.to(to).emit("peer:nego:needed", { from: socket.id, offer });
-  });
+const ioHandler = (req, res) => {
+  if (!res.socket.server.io) {
+    console.log("New Socket.io server...");
+    const io = new Server(res.socket.server, {
+      path: "/api/socket",
+      addTrailingSlash: false,
+      cors: {
+        origin: ["http://localhost:5173", "https://your-frontend.vercel.app"],
+        methods: ["GET", "POST"],
+        credentials: true,
+      },
+    });
 
-  socket.on("peer:nego:done", ({ to, ans }) => {
-    console.log("peer:nego:done", ans);
-    io.to(to).emit("peer:nego:final", { from: socket.id, ans });
-  });
-});
+    // Socket.IO event handlers
+    io.on("connection", (socket) => {
+      console.log("Socket Connected:", socket.id);
+
+      socket.on("roomjoin", (data) => {
+        const { email, room } = data;
+        emailToSocketId.set(email, socket.id);
+        socketIdToEmail.set(socket.id, email);
+        io.to(room).emit("user:joined", { email, id: socket.id });
+        socket.join(room);
+        io.to(socket.id).emit("room:join", data);
+      });
+
+      socket.on("user:call", ({ to, offer }) => {
+        io.to(to).emit("incoming:call", { from: socket.id, offer });
+      });
+
+      socket.on("call:accepted", ({ to, ans }) => {
+        io.to(to).emit("call:accepted", { from: socket.id, ans });
+      });
+
+      socket.on("peer:nego:needed", ({ to, offer }) => {
+        console.log("peer:nego:needed", offer);
+        io.to(to).emit("peer:nego:needed", { from: socket.id, offer });
+      });
+
+      socket.on("peer:nego:done", ({ to, ans }) => {
+        console.log("peer:nego:done", ans);
+        io.to(to).emit("peer:nego:final", { from: socket.id, ans });
+      });
+
+      socket.on("disconnect", () => {
+        const email = socketIdToEmail.get(socket.id);
+        emailToSocketId.delete(email);
+        socketIdToEmail.delete(socket.id);
+      });
+    });
+
+    res.socket.server.io = io;
+  }
+  res.end();
+};
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
+
+export default ioHandler;
