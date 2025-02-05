cc1590303c430c81ce5147d1dd973798d9cdbb43
Pawan Bisht <124587748+277pawan@users.noreply.github.com>
2025-02-05 12:06:04 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,35 +1,18 @@
-// index.js (in your server root directory)
 const { Server } = require("socket.io");
 const express = require("express");
-const cors = require("cors");
 const app = express();
-
-app.use(
-  cors({
-    origin: "*",
-    methods: ["GET", "POST"],
-    credentials: true,
-  })
-);
+const cors = require("cors");
+app.use(cors());
 
 require("dotenv").config();
-const server = require('http').createServer(app);
-
-const io = new Server(server, {
-  cors: {
-    origin: "*",
-    methods: ["GET", "POST"],
-    credentials: true
-  },
-  path: '/api/socketio'
+const io = new Server(process.env.PORT, {
+  cors: true,
 });
 
 const emailToSocketId = new Map();
 const socketIdToEmail = new Map();
-
 io.on("connection", (socket) => {
   console.log("Socket Connected:", socket.id);
-
   socket.on("roomjoin", (data) => {
     const { email, room } = data;
     emailToSocketId.set(email, socket.id);
@@ -38,27 +21,20 @@ io.on("connection", (socket) => {
     socket.join(room);
     io.to(socket.id).emit("room:join", data);
   });
-
   socket.on("user:call", ({ to, offer }) => {
     io.to(to).emit("incoming:call", { from: socket.id, offer });
   });
-
   socket.on("call:accepted", ({ to, ans }) => {
     io.to(to).emit("call:accepted", { from: socket.id, ans });
   });
 
   socket.on("peer:nego:needed", ({ to, offer }) => {
+    console.log("peer:nego:needed", offer);
     io.to(to).emit("peer:nego:needed", { from: socket.id, offer });
   });
 
   socket.on("peer:nego:done", ({ to, ans }) => {
+    console.log("peer:nego:done", ans);
     io.to(to).emit("peer:nego:final", { from: socket.id, ans });
   });
 });
-
-const PORT = process.env.PORT || 8000;
-server.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
-
-module.exports = { app, server };
