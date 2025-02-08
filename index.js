736a430f63aaad79ca885bf9596e86c9a84d0da8
Pawan Bisht <124587748+277pawan@users.noreply.github.com>
2025-02-08 19:29:31 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,13 +3,13 @@ const express = require("express");
 const app = express();
 const corsOptions = {
     credentials: true,
-    origin: ['http://localhost:5173', 'https://9f07-122-186-71-238.ngrok-free.app/'] // Whitelist the domains you want to allow
+    origin: ['http://localhost:5173', 'https://91fc-2401-4900-84d3-cfa1-f44-ffec-f996-2b4e.ngrok-free.app/'] // Whitelist the domains you want to allow
 };
 app.use(require("cors")(corsOptions));
 require("dotenv").config();
 const io = new Server(process.env.PORT, {
   cors: {
-    origin: ['http://localhost:5173', 'https://9f07-122-186-71-238.ngrok-free.app'],
+    origin: ['http://localhost:5173', 'https://91fc-2401-4900-84d3-cfa1-f44-ffec-f996-2b4e.ngrok-free.app/'],
     methods: ['GET', 'POST'],
     credentials: true
   }
