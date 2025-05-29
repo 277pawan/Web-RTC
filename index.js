651918248a684b5ceca75cd6805a1f5047f0d3f4
perry.pawan <perry.pawan@rubicotech.in>
2025-05-29 16:12:44 +0530

    patch: change the env url

--- index.js
+++ index.js
@@ -3,19 +3,13 @@ const express = require("express");
 const app = express();
 const corsOptions = {
   credentials: true,
-  origin: [
-    "http://localhost:5173",
-    "https://2c7b-122-186-71-238.ngrok-free.app/",
-  ], // Whitelist the domains you want to allow
+  origin: ["http://localhost:5174", `${process.env.FRONTEND_URL}`],
 };
 app.use(require("cors")(corsOptions));
 require("dotenv").config();
 const io = new Server(process.env.PORT, {
   cors: {
-    origin: [
-      "http://localhost:5173",
-      "https://2c7b-122-186-71-238.ngrok-free.app/",
-    ],
+    origin: ["http://localhost:5174", `${process.env.FRONTEND_URL}`],
     methods: ["GET", "POST"],
     credentials: true,
   },
